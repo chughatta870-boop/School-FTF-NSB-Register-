c94d52d9a8493204bfaa0bc8a38c87098f25cd5a
chughatta870-boop <chughatta870@gmail.com>
2026-06-28 06:42:45 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,75 @@
+// ===============================
+// School FTF & NSB Register PWA
+// Service Worker
+// ===============================
+
+const CACHE_NAME = "ftf-nsb-register-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+
+  event.waitUntil(
+
+    caches.keys().then((keys) => {
+
+      return Promise.all(
+
+        keys.map((key) => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
